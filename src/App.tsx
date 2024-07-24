0d55bfc274a896097a3390e55969f1de6920903f
vo huu nhan <vohuunhan1310@gmail.com>
2024-07-24 10:38:13 +0700

    feat

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { HereWallet } from '@here-wallet/core'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const login = async() =>{
+    const here = await HereWallet.connect();
+    const account = await here.signIn({ contractId: "social.near" });
+    console.log(`Hello ${account}!`);
+  }
 
   return (
     <>
@@ -18,8 +23,8 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={login}>
+          Connect
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    nodePolyfills({
+      // Include only necessary polyfills
+      include: ['buffer', 'process'],
+      globals: {
+        Buffer: true,
+        global: true,
+        process: true
+      }
+    })
+  ],
 })
